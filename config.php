f54779d1e8894bc64ef84733a1a25b643f8be527
anshukadyan03 <anshukadyan03@gmail.com>
2026-01-10 13:38:58 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,17 +1,19 @@
 <?php
 
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
 $host = getenv("MYSQLHOST");
 $user = getenv("MYSQLUSER");
 $pass = getenv("MYSQLPASSWORD");
 $db   = getenv("MYSQLDATABASE");
 $port = getenv("MYSQLPORT");
 
-$conn = mysqli_connect($host, $user, $pass, $db, $port);
+$conn = mysqli_connect($host, $user, $pass, $db, (int)$port);
 
 if (!$conn) {
     die("DB connection failed: " . mysqli_connect_error());
 }
 
-// echo "DB Connected Successfully"; // testing ke liye
+// echo "DB Connected Successfully";
 
 ?>
