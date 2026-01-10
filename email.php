b2f5afad27642599ee0c57ef05367c482f0c62b5
anshukadyan03 <anshukadyan03@gmail.com>
2026-01-10 21:57:59 +0530

    Update email.php

--- email.php
+++ email.php
@@ -20,7 +20,7 @@ function sendVerificationMail($to, $code){
         $mail->Username   = '9f993f001@smtp-brevo.com';
 
         // ✅ Yahan apni Brevo SMTP key paste karo
-        $mail->Password   = 'xsmtpsib-2538f7d37f30d89a80ad8aabeebc21affc799e1fa1fc477cbe66498db6bad7c6-OZ5VLb1tbLmvG3l9';
+        $mail->Password   = 'xsmtpsib-2538f7d37f30d89a80ad8aabeebc21affc799e1fa1fc477cbe66498db6bad7c6-u6zyx5Gxhmw9hbAD';
 
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port       = 587;
@@ -83,3 +83,4 @@ function sendVerificationMail($to, $code){
         return 'Mailer Error: ' . $mail->ErrorInfo;
     }
 }
+
